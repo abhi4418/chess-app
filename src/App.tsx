521eabf470c3cb65e21d889580c0695323ac6377
Rajat gupta <132932825+rajatgupta2004@users.noreply.github.com>
2024-03-24 14:07:01 +0530

    video_2

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Chessboard from './Components/Chessboard/Chessboard'
+import './App.css'
+function App() {
+  return (
+    <div id = "app">
+      <Chessboard/>
+    </div>
+  )
+}
+
+export default App
